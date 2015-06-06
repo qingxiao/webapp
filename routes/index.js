5e332a7b661b30f97cc5a9d2ff163849025f38f4
xiaoqing <qing.xiao@tcl.com>
2015-06-06 17:34:45 +0800

    modify

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,13 @@ var spotifyApi = require('../module/spotifyApi');
 /* GET home page. */
 router.get('/', function (req, res, next) {
     spotifyApi.getCategories({
-        limit: 50,
+        limit: 20,
         offset: 0,
         country: 'SE',
         locale: 'sv_SE'
     })
         .then(function (data) {
-            console.log(JSON.stringify(data.body));
+            //res.send(data.body);
             res.render('index', data.body);
             // next();
         }, function (err) {
@@ -36,41 +36,9 @@ router.get('/category/:cid', function (req, res, next) {
         },function(err){
             console.log("Something went wrong1!", err);
         });
-    ;
-   /* spotifyApi.getCategory(cid)
-        .then(function (data) {
-            console.log(data.body);
-        }, function (err) {
-            console.log("Something went wrong1!", err);
-        });
-    spotifyApi.getPlaylistsForCategory(cid, {
-        limit: 20,
-        offset: 0
-    })
-        .then(function (data) {
-            console.log(data.body);
-        }, function (err) {
-            console.log("Something went wrong2!", err);
-        });
-*/
+
 });
 router.get('/callback', function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
-function getIndex() {
-
-    spotifyApi.getCategories({
-        limit: 5,
-        offset: 0,
-        country: 'SE',
-        locale: 'sv_SE'
-    })
-        .then(function (data) {
-            console.log(data.body);
-        }, function (err) {
-            console.log("Something went wrong!", err);
-        });
-
-
-}
 module.exports = router;
