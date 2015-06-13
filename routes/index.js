558fdd2d2b9912924802f69b37ba50f612fd472e
xiaoqing <qing.xiao@tcl.com>
2015-06-13 16:52:54 +0800

    track

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.get('/category/:cid', function (req, res, next) {
         spotifyApi.getPlaylistsForCategory(cid, {limit:5})
     ])
         .then(function(datas){
-            console.log(JSON.stringify(datas[0].body), JSON.stringify(datas[1].body));
+            //console.log(JSON.stringify(datas[0].body), JSON.stringify(datas[1].body));
             res.render('page/category/list', {
                 category:datas[0].body,
                 playList:datas[1].body
@@ -38,6 +38,27 @@ router.get('/category/:cid', function (req, res, next) {
         });
 
 });
+
+router.get('/album/:albumId', function (req, res, next) {
+    var albumId = req.params.albumId;
+    //第一个参数 uiserid 没有就默认 spotivy
+    spotifyApi.getPlaylistTracks('spotify', albumId, {limit:10})
+        .then(function(data) {
+            console.log(JSON.stringify(data.body))
+            res.render('page/playlist', {aData:data.body});
+        }, function(res){console.log(res)});
+
+});
+router.get('/track/:id', function (req, res, next) {
+    var id = req.params.id;
+    spotifyApi.getTrack (id)
+        .then(function(data) {
+            console.log(JSON.stringify(data.body))
+           // res.render('page/playlist', {aData:data.body});
+        }, function(res){console.log(res)});
+
+});
+
 router.get('/callback', function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
